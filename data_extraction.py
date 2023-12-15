cccf5f5fcdc020ab88a470ef6d1738c8335c20e3
Spencer Duvwiama <ejduv@yahoo.co.uk>
2023-12-15 17:47:51 +0000

    Modified print statement in read_rds_table function

--- data_extraction.py
+++ data_extraction.py
@@ -32,28 +32,8 @@ class DataExtractor:
             query = f"SELECT * FROM {table_name};"
             df = pd.read_sql_query(query, db_engine)
 
-            print(f"Data extracted from the '{table_name}' table.")
+            print(f"Data extracted from the '{table_name}' table and into a DataFrame.")
             return df
         except Exception as e:
             print(f"Error reading table '{table_name}': {e}")
             return pd.DataFrame() # Return an empty DataFrame if error
-
-
-# Create an instance of the DatabaseConnector class
-connector = DatabaseConnector()
-
-    
-# Create an instance of the DataExtractor class, passing the DatabaseConnector as an argument
-data_extractor = DataExtractor(db_connector=connector)
-    
-# Use the DatabaseConnector to get the list of tables
-tables = connector.list_db_tables()
-
-# The table that contains user data
-legacy_users_table = 'legacy_users'
-# Use the DataExtractor class to read data from the 'legacy_user' table
-legacy_users_df = data_extractor.read_rds_table(legacy_users_table)
-
-#print(legacy_users_df.head())
-
-legacy_users_df.info()
